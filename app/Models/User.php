faca5ca0fde1152c872c159b9c3fe9e1b4951bee
Bikas chaudhary tharu <vcbikash123@gmail.com>
2020-12-18 14:40:49 +0545

    Adding JWT package in project for authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
 
     /**
      * The attributes that should be hidden for arrays.
@@ -40,4 +42,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        // TODO: Implement getJWTIdentifier() method.
+        return $this->getKey();
+
+
+    }
+
+    public function getJWTCustomClaims()
+    {
+        // TODO: Implement getJWTCustomClaims() method.
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ use App\Http\Controllers\PostController;
 
 //short way to defining route resource controller
 
-Route::apiResource('/posts',PostController::class);
+//Route::apiResource('/posts',PostController::class);
 
 //This is long method of defining the route
 //Route::get('/posts',[PostController::class, 'index'] );
@@ -49,3 +49,57 @@ Route::apiResource('/posts',PostController::class);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//create a user route
+
+Route::get('/user-create',function (Request $request){
+    App\Models\User::create([
+        'name'=>'Bikas chaudhary tharu',
+        'email'=>'vcbikash123@gmail.com',
+        'password'=>\Illuminate\Support\Facades\Hash::make('chaudharypassword'),
+    ]);
+
+
+});
+
+Route::post('/login',function (Request $request){
+//    $credentials =[
+//
+//        'email'=>'vcbikash123@gmail.com',
+//        'password'=>'chaudharypassword'
+//    ];
+    $credentials = request()->only(['email','password']);
+    $token = auth()->attempt($credentials);
+    return $token;
+});
+
+
+Route::middleware('auth')->get('/me', function (){
+   return auth()->user();
+});
+
+Route::middleware('auth')->apiResource('/posts',PostController::class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
